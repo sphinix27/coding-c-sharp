611a7bf316e31d59738a4c0c3df9429cd5de3a10
Abel Barrientos <abelbs27101@gmail.com>
2017-05-28 11:25:47 -0400

    Agregar archivos de proyecto.

---
+++ LibraryTests/LibraryTest.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Coding;
+
+namespace LibraryTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
